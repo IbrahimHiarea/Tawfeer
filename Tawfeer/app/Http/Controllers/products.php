7c4cceccc5512a4ad39c03e2a836b01933ceafb1
Ibrahim <ibrahimhiarea@gmail.com>
2021-12-04 20:11:38 +0200

    Add Errors handling for all function

--- Tawfeer/app/Http/Controllers/products.php
+++ Tawfeer/app/Http/Controllers/products.php
@@ -4,36 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
- 
+use Illuminate\Support\Facades\Validator;
+
 class products extends Controller
 {
-
-    public function store(Request $request){
-        $product = new Product();
-
-        $product->name = $request->input('name');
-        $product->description = $request->input('description');
-        $product->expiryDate = $request->input('expiryDate');
-        $product->mainPrice = $request->input('mainPrice');
-        $product->imgUrl = $request->input('imgUrl');
-        $product->quantity = $request->input('quantity');
-        $product->category = $request->input('category');
-
-        // Don't Forget The Owner ID from the Token in the Header
-        $product->ownerId = $request->header('ownerId');
-
-        $product->date1 = $request->input('date1');
-        $product->price1 = $request->input('price1');
-        $product->date2 = $request->input('date2');
-        $product->price2 = $request->input('price2');
-        $product->date3 = $request->input('date3');
-        $product->price3 = $request->input('price3');
-
-        $product->save();
-
-        return response()->json(['message' => 'The Product has benn added successfuly'],200);
-    }
-
 
     // private function calcPrice($price1 , $price2 , $price3 , $date1 , $date2 , $date3){
     //     //the Curent Date
@@ -43,12 +17,12 @@ class products extends Controller
 
 
     // }
-    public function indx(){
+    public function index(){
         // Get The Products by newest
         $product = Product::orderBy('created_at' , 'desc')->get();
 
         // $curPrice = calcPrice()
-        
+
         $jsoncontnet = json_decode($product , true);
         return response()->json([
             'messaeg' => "The List Of Product : ",
@@ -56,12 +30,55 @@ class products extends Controller
         ]);
     }
 
+    public function store(Request $request){
+
+        $valid = Validator::make($request->all() , [
+            'name' => ['required' , 'string'],
+            'description' => ['string'],
+            'expiryDate' => ['required'],
+            'mainPrice' => ['required'],
+            'quantity' => ['required'],
+            'category' => ['required' , 'string'],
+        ]);
+
+        if($valid->fails()){
+            return response()->json($valid->errors()->all());
+        }
+
+        $product = new Product();
+
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->expiryDate = $request->input('expiryDate');
+        $product->mainPrice = $request->input('mainPrice');
+        $product->imgUrl = $request->input('imgUrl');
+        $product->quantity = $request->input('quantity');
+        $product->category = $request->input('category');
+        // Don't Forget The Owner ID from the Token in the Header
+        $product->ownerId = $request->header('ownerId');
+        $product->date1 = $request->input('date1');
+        $product->price1 = $request->input('price1');
+        $product->date2 = $request->input('date2');
+        $product->price2 = $request->input('price2');
+        $product->date3 = $request->input('date3');
+        $product->price3 = $request->input('price3');
+
+        $product->save();
+
+        return response()->json(['message' => 'The Product has benn added successfully'],200);
+    }
+
 
     public function show($productId){
-        $product = Product::findOrFail($productId);
+        // Get The Product
+        $product = Product::find($productId);
 
-        // Don't Forget to handel the variabel that you want to send 
+        // Handling Wrong ID
+        if(!$product){
+            return response()->json(['message' => 'Invalid ID']);
+        }
 
+        // Don't Forget to handel the variables that you want to send
         $jsoncontnet = json_decode($product , true);
         return response()->json([
             "Products" => $jsoncontnet
@@ -69,6 +86,14 @@ class products extends Controller
     }
 
     public function search(Request $request){
+        $valid = Validator::make($request->all() , [
+            'searchBy' => ['required' , 'string'],
+            'search' => ['required' , 'string'],
+        ]);
+
+        if($valid->fails()){
+            return response()->json($valid->errors()->all());
+        }
         $searchBy = $request->input('searchBy');
         $search = $request->input('search');
 
@@ -81,10 +106,20 @@ class products extends Controller
 
 
     public function destroy($productId){
-        // First Leaen How to dlete from data Base :)
+        // Get the product where the id is equal to productId
+        $product = Product::find($productId);
+
+        if(!$product){
+            return response()->json(['message' => 'Invalid ID']);
+        }
+
+        // Delete it
+        $product->delete();
+        return response()->json(['message' => 'The Product Has Been Delete successfully']);
     }
 
-    public function update($productId){
-        
+    public function update(Request $request,$productId){
+
     }
 }
+
--- Tawfeer/app/Http/Controllers/users.php
+++ Tawfeer/app/Http/Controllers/users.php
@@ -6,17 +6,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
+
 
 
 class users extends Controller
 {
     public function register(Request $request){
 
-        // make A conditions
+        // make a conditions
         $valid = Validator::make($request->all() , [
             'name' => ['required' , 'string' , 'max:25'] ,
             'email' => ['required' , 'string' , 'email' , 'max:50' , 'unique:users'],
@@ -43,26 +44,8 @@ class users extends Controller
         return response()->json(['message' => 'Welcome :)'],200);
         //Don't forget to return Token !!!
     }
-}
 
+    public function login(Request $request){
 
-/*public function update(Request $request,$id){
-    if(User::where('id', $id)->exists()){
-        $user = User::find($id);
-
-        $user->fullName = !empty($request->input('fullName')) ? $request->input('fullName') : $user->fullName;
-        $user->email = !empty($request->input('email')) ? $request->input('email') : $user->email;
-        $user->password = !empty($request->input('password')) ? $request->input('password') : $user->password;
-        $user->phoneNumber = !empty($request->input('phoneNumber')) ? $request->input('phoneNumber') : $user->phoneNumber;
-        $user->imgUrl = !empty($request->input('imgUrl')) ? $request->input('imgUrl') : $user->imgUrl;
-        $user->save();
-        return response()-> json([
-            'message'=>'Updated Successfully'
-        ], 200);
-    }else{
-        return response([
-            'status'=> 0,
-            'message'=> 'User not found'
-        ],404);
     }
-}*/
+}
